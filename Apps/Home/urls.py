fd464792a5e2248335ef4cb9cab97e01d44793d0
LuliVel <lulibea2910@gmail.com>
2025-11-20 20:50:15 -0600

    .

--- Apps/Home/urls.py
+++ Apps/Home/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='homeapp'),
     path('creditos/', views.CreditosView.as_view(), name='creditos'),
     path('usuarios/', views.UsuariosView.as_view(), name='usuarios'),
+    path('usuarios/<int:pk>/', views.VerUsuarioView.as_view(), name='ver_usuario'),
+    path('usuarios/<int:pk>/eliminar/', views.EliminarUsuarioView.as_view(), name='eliminar_usuario'),
     path('registro/', views.RegistroView.as_view(), name='registro'),
     path('login/', views.LoginView.as_view(), name='login'),
+    
 ]
--- Apps/Home/views.py
+++ Apps/Home/views.py
@@ -1,25 +1,42 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-
-# Create your views here.
-class CreditosView(TemplateView):
-    template_name = 'creditos.html'
-
-class HomeView(TemplateView):
-    template_name = 'index.html'
-
-from django.views.generic import ListView, CreateView, FormView
+from django.views.generic import DetailView, DeleteView
+from django.contrib import messages
+from django.views.generic import TemplateView, ListView, CreateView, FormView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django import forms
 from django.urls import reverse_lazy
 from .models import Cliente
 
-class UsuariosView(ListView):
+# Create your views here.
+class CreditosView(LoginRequiredMixin, TemplateView):
+    template_name = 'creditos.html'
+    login_url = 'Home:login'
+
+class HomeView(TemplateView):
+    template_name = 'home.html'
+
+
+class UsuariosView(LoginRequiredMixin, ListView):
     model = Cliente
     template_name = 'usuarios.html'
     context_object_name = 'clientes'
+    login_url = 'Home:login'
+
+class RegistroForm(UserCreationForm):
+    telefono = forms.CharField(max_length=20, required=False, label='Teléfono')
+
+    class Meta(UserCreationForm.Meta):
+        fields = UserCreationForm.Meta.fields + ('telefono',)
+
+    def save(self, commit=True):
+        user = super().save(commit)
+        telefono = self.cleaned_data.get('telefono', '')
+        if commit:
+            Cliente.objects.filter(perfil=user).update(telefono=telefono)
+        return user
 
 class RegistroView(CreateView):
-    form_class = UserCreationForm
+    form_class = RegistroForm
     template_name = 'registro.html'
     success_url = reverse_lazy('Home:login')
 
@@ -27,3 +44,16 @@ class LoginView(FormView):
     form_class = AuthenticationForm
     template_name = 'login.html'
     success_url = reverse_lazy('Home:homeapp')
+class VerUsuarioView(DetailView):
+    model = Cliente
+    template_name = 'ver_usuario.html'
+    context_object_name = 'cliente'
+
+class EliminarUsuarioView(DeleteView):
+    model = Cliente
+    template_name = 'eliminar_usuario.html'
+    success_url = reverse_lazy('Home:usuarios')
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(request, "Usuario eliminado correctamente.")
+        return super().delete(request, *args, **kwargs)
